811d5d191d950d77188a8e4f2bb9a86936dab0ef
Zorba11 <zorbathegreek111@gmail.com>
2021-09-08 18:48:16 -0600

    views incomplete, just focus on ORM stuff

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 
 // Database
 const db = require('./config/database');
+
 // Test DB
 
 db.authenticate()
@@ -11,6 +12,13 @@ db.authenticate()
   .catch((err) => console.error(err));
 
 const app = express();
+
+// Handlebars
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => res.send('INDEX'));
 
--- routes/gigs.js
+++ routes/gigs.js
@@ -3,13 +3,42 @@ const router = express.Router();
 const db = require('../config/database');
 const Gig = require('../model/Gig');
 
+// GET gig list
 router.get('/', (req, res) =>
   Gig.findAll()
     .then((gigs) => {
-      console.log(gigs);
-      res.sendStatus(200);
+      res.render('gigs', {
+        gigs,
+      });
     })
     .catch((err) => console.log(err))
 );
+
+// Display add gig form
+
+// Add a gig list
+router.post('/add', (req, res) => {
+  const data = {
+    title: 'Simple WP website',
+    technologies: 'Php, html, css',
+    budget: '$1000',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, ',
+    contact_email: 'some@gmail.com',
+  };
+
+  let { title, technologies, budget, description, contact_email } = data;
+
+  // Insert into Tablets
+  Gig.create({
+    title,
+    technologies,
+    description,
+    budget,
+    contact_email,
+  })
+    .then((gig) => res.redirect('/gigs'))
+    .catch((err) => console.log(err));
+});
 
 module.exports = router;
